306e0dff2de1b6f2f6b158cccec62468610b7592
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2021-11-01 01:14:59 +0800

    revised

--- prog3v2.py
+++ prog3v2.py
@@ -1,3 +1,4 @@
+import math
 def money():
     _money = int(input("Enter the amount of money you have: "))
     return _money
@@ -11,13 +12,13 @@ def operation2(moneyF, appleF):
     return apple_total
 
 def operation(moneyF, appleF):
-    apple_total = moneyF/appleF
-    cost_total = appleF*apple_total
+    apple_quant = int(moneyF/appleF)
+    cost_total = apple_quant*appleF
     money_change = moneyF - cost_total
     return money_change
 
 moneyq = money()
 appleq = apple()
-total = operation2(moneyq,appleq)
-change = operation(moneyq, appleq)
+total = math.floor(operation2(moneyq,appleq))
+change = float(operation(moneyq, appleq))
 print(f"You can buy {total} apples and your change is PHP{change}. Thank you!")
